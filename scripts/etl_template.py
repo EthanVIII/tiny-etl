5b313af9004d318a7ac24980cbb2884b7312a84a
ethan <u6368029@anu.edu.au>
2024-01-01 21:47:41 +1100

    Layout rough structure for the data pipeline. WIP:

---
+++ scripts/etl_template.py
@@ -0,0 +1,11 @@
+# All data sources should have the following three functions for the ETL process.
+
+def extract_data():
+    pass
+
+def transform_data(data):
+    pass
+
+def load_data(data, cursor, connection):
+    pass
+
